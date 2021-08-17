1d9016d24c910d6f8c921fda981de38ce1e20d8e
Artem Popov <aptem336@gmail.com>
2021-08-17 21:54:02 +0800

    simplify

--- src/main/java/Drawable.java
+++
@@ -1,6 +0,0 @@
-import com.jogamp.opengl.GLAutoDrawable;
-
-public interface Drawable {
-    void draw(GLAutoDrawable drawable);
-    float[] getColor();
-}
--- src/main/java/RasterScan.java
+++ src/main/java/RasterScan.java
@@ -10,10 +10,15 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RasterScan implements GLEventListener {
-    private Scene scene;
+
+    private static final List<Point> POINTS = new ArrayList<>();
+    private static float[] colors;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -37,15 +42,12 @@ public class RasterScan implements GLEventListener {
 
             @Override
             public void mouseReleased(MouseEvent mouseEvent) {
-                int X_ROUNDER = 20;
-                int Y_ROUNDER = 20;
                 if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
-                    Polygon.POINTS.add(new Point(mouseEvent.getX() - mouseEvent.getX() % X_ROUNDER + X_ROUNDER / 2,
-                            mouseEvent.getY() - mouseEvent.getY() % Y_ROUNDER + Y_ROUNDER / 2));
-                    Arrays.fill(Polygon.colors, 0.0F);
+                    POINTS.add(mouseEvent.getPoint());
+                    Arrays.fill(colors, 0.0F);
                 } else if (mouseEvent.getButton() == MouseEvent.BUTTON3) {
-                    Polygon.POINTS.clear();
-                    Arrays.fill(Polygon.colors, 0.0F);
+                    POINTS.clear();
+                    Arrays.fill(colors, 0.0F);
                 }
             }
 
@@ -74,7 +76,7 @@ public class RasterScan implements GLEventListener {
         GL2 gl = drawable.getGL().getGL2();
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        scene = new Scene(drawable);
+        colors = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 4];
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -85,9 +87,43 @@ public class RasterScan implements GLEventListener {
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-        scene.draw(drawable);
+        gl.glClearColor(0.15F, 0.15F, 0.15F, 0.15F);
+        Arrays.fill(RasterScan.colors, 0.0F);
+        for (int i = 0; i < POINTS.size(); i++) {
+            Point a = POINTS.get(i);
+            Point b = POINTS.get((i + 1) % POINTS.size());
+            if (a.y > b.y) {
+                a = POINTS.get((i + 1) % POINTS.size());
+                b = POINTS.get(i);
+            }
+            for (int y = a.y; y < b.y; y++) {
+                int startX = a.x + ((y - a.y) * (b.x - a.x)) / (b.y - a.y);
+                for (int x = startX; x < drawable.getSurfaceWidth(); x++) {
+                    int offset = (drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 4 + x * 4;
+                    float[] color = getColor(offset);
+                    if (color[0] == 1.0F && color[1] == 1.0F && color[2] == 1.0F && color[3] == 1.0F) {
+                        setColor(new float[]{0.0F, 0.0F, 0.0F, 0.0F}, offset);
+                    } else {
+                        setColor(new float[]{1.0F, 1.0F, 1.0F, 1.0F}, offset);
+                    }
+                }
+            }
+        }
+        gl.glDrawPixels(drawable.getSurfaceWidth(), drawable.getSurfaceHeight(), GL2.GL_RGBA, GL2.GL_FLOAT, FloatBuffer.wrap(RasterScan.colors));
     }
+
 
     public void dispose(GLAutoDrawable glAutoDrawable) {
     }
+
+    private float[] getColor(int offset) {
+        float[] color = new float[4];
+        System.arraycopy(RasterScan.colors, offset, color, 0, 4);
+        return color;
+    }
+
+    public void setColor(float[] color, int offset) {
+        System.arraycopy(color, 0, RasterScan.colors, offset, 4);
+    }
+
 }
