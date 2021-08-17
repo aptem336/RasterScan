9d4de42f2b753b1aab8f8eb0526391f71b619bbe
Artem Popov <aptem336@gmail.com>
2021-08-17 22:21:46 +0800

    RGBA -> RGB

--- src/main/java/RasterScan.java
+++ src/main/java/RasterScan.java
@@ -29,7 +29,7 @@ public class RasterScan implements GLEventListener {
 
         GLCanvas canvas = new GLCanvas();
         canvas.addGLEventListener(new RasterScan());
-        canvas.setSize(size);
+        canvas.setSize(frame.getSize());
         canvas.setLocation(0, 0);
 
         canvas.addMouseListener(new MouseListener() {
@@ -75,13 +75,13 @@ public class RasterScan implements GLEventListener {
         GL2 gl = drawable.getGL().getGL2();
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 4];
+        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 3];
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL2 gl = drawable.getGL().getGL2();
         gl.glOrthof(0, width, height, 0, 0, 1);
-        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 4];
+        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 3];
     }
 
     public void display(GLAutoDrawable drawable) {
@@ -91,6 +91,7 @@ public class RasterScan implements GLEventListener {
         for (int i = 0; i < points.size(); i++) {
             Point a = points.get(i);
             Point b = points.get((i + 1) % points.size());
+            //Q
             if (a.y > b.y) {
                 a = points.get((i + 1) % points.size());
                 b = points.get(i);
@@ -98,22 +99,22 @@ public class RasterScan implements GLEventListener {
             for (int y = a.y; y < b.y; y++) {
                 int startX = a.x + ((y - a.y) * (b.x - a.x)) / (b.y - a.y);
                 for (int x = startX; x < drawable.getSurfaceWidth(); x++) {
-                    int offset = (drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 4 + x * 4;
-                    float[] color = getColor(offset);
-                    if (color[0] == 1.0F && color[1] == 1.0F && color[2] == 1.0F && color[3] == 1.0F) {
-                        setColor(new float[]{0.15F, 0.15F, 0.15F, 0.15F}, offset);
+                    int offset = (drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 3 + x * 3;
+                    float[] color = getPixel(offset);
+                    if (color[0] == 1.0F && color[1] == 1.0F && color[2] == 1.0F) {
+                        setPixel(new float[]{0.15F, 0.15F, 0.15F}, offset);
                     } else {
-                        setColor(new float[]{1.0F, 1.0F, 1.0F, 1.0F}, offset);
+                        setPixel(new float[]{1.0F, 1.0F, 1.0F}, offset);
                     }
                 }
             }
         }
-        gl.glDrawPixels(drawable.getSurfaceWidth(), drawable.getSurfaceHeight(), GL2.GL_RGBA, GL2.GL_FLOAT, FloatBuffer.wrap(pixels));
+        gl.glDrawPixels(drawable.getSurfaceWidth(), drawable.getSurfaceHeight(), GL2.GL_RGB, GL2.GL_FLOAT, FloatBuffer.wrap(pixels));
         //Q
         if (!points.isEmpty()) {
             gl.glPointSize(10.0F);
             gl.glBegin(GL2.GL_POINTS);
-            gl.glColor4f(1.0F, 0.0F, 0.0F, 1.0F);
+            gl.glColor3f(1.0F, 0.0F, 0.0F);
             gl.glVertex2i(points.get(points.size() - 1).x, points.get(points.size() - 1).y);
             gl.glEnd();
         }
@@ -123,14 +124,14 @@ public class RasterScan implements GLEventListener {
     public void dispose(GLAutoDrawable glAutoDrawable) {
     }
 
-    private float[] getColor(int offset) {
-        float[] color = new float[4];
-        System.arraycopy(RasterScan.pixels, offset, color, 0, 4);
+    private float[] getPixel(int offset) {
+        float[] color = new float[3];
+        System.arraycopy(RasterScan.pixels, offset, color, 0, 3);
         return color;
     }
 
-    public void setColor(float[] color, int offset) {
-        System.arraycopy(color, 0, RasterScan.pixels, offset, 4);
+    public void setPixel(float[] color, int offset) {
+        System.arraycopy(color, 0, RasterScan.pixels, offset, 3);
     }
 
 }
