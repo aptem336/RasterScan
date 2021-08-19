9907dc110f550e521e4e213e234d473257c085d9
Artem Popov <aptem336@gmail.com>
2021-08-19 09:58:56 +0800

    Bresenham's with anti-aliasing

--- src/main/java/RasterScan.java
+++ src/main/java/RasterScan.java
@@ -1,3 +1,4 @@
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
@@ -16,9 +17,9 @@ import java.util.List;
 
 public class RasterScan implements GLEventListener, MouseListener {
 
-    private final float[] borderColor = new float[]{1.0F, 0.0F, 0.0F};
-    private final float[] fillColor = new float[]{1.0F, 1.0F, 1.0F};
-    private final float[] clearColor = new float[]{0.15F, 0.15F, 0.15F};
+    private final float[] borderColor = new float[]{1.0F, 0.0F, 0.0F, 1.0F};
+    private final float[] fillColor = new float[]{1.0F, 0.0F, 0.0F, 1.0F};
+    private final float[] clearColor = new float[]{0.15F, 0.15F, 0.15F, 1.0F};
     private final List<Point> points = new ArrayList<>();
     private float[] pixels;
 
@@ -50,15 +51,20 @@ public class RasterScan implements GLEventListener, MouseListener {
     }
 
     public void init(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glEnable(GL2.GL_BLEND);
+        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 3];
+        pixels = new float[drawable.getSurfaceWidth() * drawable.getSurfaceHeight() * 4];
     }
 
     public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         for (int i = 0; i < pixels.length; i++) {
-            pixels[i] = clearColor[i % 3];
+            pixels[i] = clearColor[i % 4];
         }
         for (int i = 0; i < points.size(); i++) {
             Point a = points.get(i);
@@ -70,49 +76,54 @@ public class RasterScan implements GLEventListener, MouseListener {
             for (int y = a.y; y < b.y; y++) {
                 int x = a.x + ((y - a.y) * (b.x - a.x)) / (b.y - a.y);
                 for (int j = x; j < drawable.getSurfaceWidth(); j++) {
-                    invertColor((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 3 + j * 3);
+                    invertColor((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 4 + j * 4);
                 }
-            }
-            int dx = b.x - a.x;
-            int dy = b.y - a.y;
-            int signX = Integer.compare(dx, 0);
-            int signY = Integer.compare(dy, 0);
-            if (dx < 0) dx = -dx;
-            if (dy < 0) dy = -dy;
-            int pdx;
-            int pdy;
-            int es;
-            int el;
-            if (dx > dy) {
-                pdx = signX;
-                pdy = 0;
-                es = dy;
-                el = dx;
-            } else {
-                pdx = 0;
-                pdy = signY;
-                es = dx;
-                el = dy;
-            }
-            int x = a.x;
-            int y = a.y;
-            int err = el / 2;
-            fillPixel((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 3 + x * 3, borderColor);
-            for (int t = 0; t < el; t++) {
-                err -= es;
-                if (err < 0) {
-                    err += el;
-                    x += signX;
-                    y += signY;
-                } else {
-                    x += pdx;
-                    y += pdy;
-                }
-                fillPixel((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 3 + x * 3, borderColor);
             }
         }
-        GL2 gl = drawable.getGL().getGL2();
-        gl.glDrawPixels(drawable.getSurfaceWidth(), drawable.getSurfaceHeight(), GL2.GL_RGB, GL2.GL_FLOAT, FloatBuffer.wrap(pixels));
+        for (int i = 0; i < points.size(); i++) {
+            Point a = points.get(i);
+            Point b = points.get((i + 1) % points.size());
+            int I = 16;
+            int x = b.x;
+            int y = b.y;
+            int dx = a.x - b.x;
+            int dy = a.y - b.y;
+            int sx = Integer.compare(dx, 0);
+            int sy = Integer.compare(dy, 0);
+            dx = Math.abs(dx);
+            dy = Math.abs(dy);
+            float m = (float) dy / dx;
+            boolean flag = false;
+            if (m > 1) {
+                int temp = dx;
+                dx = dy;
+                dy = temp;
+                m = 1.0F / m;
+                flag = true;
+            }
+            m *= I;
+            float f = I / 2.0F;
+            float w = I - m;
+            borderColor[3] = f / I;
+            fillPixel((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 4 + x * 4, borderColor);
+            for (int j = 1; j < dx; j++) {
+                if (f < w) {
+                    if (flag) {
+                        y = y + sy;
+                    } else {
+                        x = x + sx;
+                    }
+                    f = f + m;
+                } else {
+                    x = x + sx;
+                    y = y + sy;
+                    f = f - w;
+                }
+                borderColor[3] = f / I;
+                fillPixel((drawable.getSurfaceHeight() - y) * drawable.getSurfaceWidth() * 4 + x * 4, borderColor);
+            }
+        }
+        gl.glDrawPixels(drawable.getSurfaceWidth(), drawable.getSurfaceHeight(), GL2.GL_RGBA, GL2.GL_FLOAT, FloatBuffer.wrap(pixels));
     }
 
 
@@ -120,9 +131,10 @@ public class RasterScan implements GLEventListener, MouseListener {
     }
 
     private void invertColor(int offset) {
-        if (pixels[offset] == fillColor[1]
+        if (pixels[offset] == fillColor[0]
                 && pixels[offset + 1] == fillColor[1]
-                && pixels[offset + 2] == fillColor[2]) {
+                && pixels[offset + 2] == fillColor[2]
+                && pixels[offset + 3] == fillColor[3]) {
             fillPixel(offset, clearColor);
         } else {
             fillPixel(offset, fillColor);
@@ -130,7 +142,7 @@ public class RasterScan implements GLEventListener, MouseListener {
     }
 
     private void fillPixel(int offset, float[] color) {
-        System.arraycopy(color, 0, pixels, offset, 3);
+        System.arraycopy(color, 0, pixels, offset, 4);
     }
 
     @Override
